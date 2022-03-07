5856fdd892864764204e1492887dae058253cceb
Fedor <lfa1@yandex.ru>
2022-03-07 18:45:16 +0300

    Update http.h

--- user/include/http.h
+++ user/include/http.h
@@ -1,6 +1,9 @@
 #ifndef USER_INCLUDE_HTTP_H_
 #define USER_INCLUDE_HTTP_H_
 
+#define OTA_BUF_LEN 512
+#define HTML_PATH "/html"
+
 extern HttpdBuiltInUrl builtInUrls[];
 
 int ICACHE_FLASH_ATTR cgi_upload (HttpdConnData *connData);
