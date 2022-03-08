e51cefcd5ce4aa0064632bbc7c7f298501a7aab0
Fedor <lfa1@yandex.ru>
2022-03-09 00:22:45 +0300

    Update http.h

--- user/include/http.h
+++ user/include/http.h
@@ -6,7 +6,8 @@
 
 extern HttpdBuiltInUrl builtInUrls[];
 
-int ICACHE_FLASH_ATTR cgi_upload (HttpdConnData *connData);
-int ICACHE_FLASH_ATTR cgi_response (HttpdConnData *connData);
+int ICACHE_FLASH_ATTR cgi_upload(HttpdConnData *connData);
+int ICACHE_FLASH_ATTR cgi_response(HttpdConnData *connData);
+int ICACHE_FLASH_ATTR cgi_list(HttpdConnData *connData);
 
 #endif /* USER_INCLUDE_HTTP_H_ */
