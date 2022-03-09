0a7ccabd6757ea5a309aacb47aa38ba141f75ad9
Fedor <lfa1@yandex.ru>
2022-03-09 15:40:25 +0300

    Update appdef.h

--- user/include/appdef.h
+++ user/include/appdef.h
@@ -1,14 +1,27 @@
 #ifndef USER_INCLUDE_APPDEF_H_
 #define USER_INCLUDE_APPDEF_H_
 
-#ifndef OTA_FLASH_MAP
-#define OTA_FLASH_MAP 6
-#define OTA_FLASH_SIZE_K 4096
-#endif
-
 #ifndef SPI_FLASH_SIZE_MAP
 #define SPI_FLASH_SIZE_MAP 6
 #endif
+
+#if SPI_FLASH_SIZE_MAP ==   0
+#define OTA_FLASH_SIZE_K    512
+#elif SPI_FLASH_SIZE_MAP == 2
+#define OTA_FLASH_SIZE_K    1024
+#elif SPI_FLASH_SIZE_MAP == 3 || SPI_FLASH_SIZE_MAP == 5
+#define OTA_FLASH_SIZE_K    2048
+#elif SPI_FLASH_SIZE_MAP == 4 || SPI_FLASH_SIZE_MAP == 6
+#define OTA_FLASH_SIZE_K    4096
+#elif SPI_FLASH_SIZE_MAP == 8
+#define OTA_FLASH_SIZE_K    8192
+#elif SPI_FLASH_SIZE_MAP == 9
+#define OTA_FLASH_SIZE_K    16384
+#else
+#error Not support this flash size map
+#endif
+
+
 
 typedef enum {
     GPIO0 = 0,
