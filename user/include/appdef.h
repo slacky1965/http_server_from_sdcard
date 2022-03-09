77fb8b09d2fbc13ab8a61baf922d64e5df9148a0
Fedor <lfa1@yandex.ru>
2022-03-09 17:50:09 +0300

    Update appdef.h

--- user/include/appdef.h
+++ user/include/appdef.h
@@ -5,22 +5,20 @@
 #define SPI_FLASH_SIZE_MAP 6
 #endif
 
-#if SPI_FLASH_SIZE_MAP ==   0
-#define OTA_FLASH_SIZE_K    512
-#elif SPI_FLASH_SIZE_MAP == 2
-#define OTA_FLASH_SIZE_K    1024
-#elif SPI_FLASH_SIZE_MAP == 3 || SPI_FLASH_SIZE_MAP == 5
-#define OTA_FLASH_SIZE_K    2048
-#elif SPI_FLASH_SIZE_MAP == 4 || SPI_FLASH_SIZE_MAP == 6
-#define OTA_FLASH_SIZE_K    4096
-#elif SPI_FLASH_SIZE_MAP == 8
-#define OTA_FLASH_SIZE_K    8192
-#elif SPI_FLASH_SIZE_MAP == 9
-#define OTA_FLASH_SIZE_K    16384
-#else
-#error Not support this flash size map
+#ifndef OTA_FLASH_SIZE_K
+#define OTA_FLASH_SIZE_K 4096
 #endif
 
+#if SPI_FLASH_SIZE_MAP == 3
+#define FIRMWARE_POS      ((OTA_FLASH_SIZE_K/2)*1024/2)+0x1000
+#define FIRMWARE_SIZE     ((OTA_FLASH_SIZE_K/2)*1024/2)-0x1000
+#elif SPI_FLASH_SIZE_MAP == 4
+#define FIRMWARE_POS      ((OTA_FLASH_SIZE_K/4)*1024/2)+0x1000
+#define FIRMWARE_SIZE     ((OTA_FLASH_SIZE_K/4)*1024/2)-0x1000
+#else
+#define FIRMWARE_POS      ((OTA_FLASH_SIZE_K*1024)/2)+0x1000
+#define FIRMWARE_SIZE     ((OTA_FLASH_SIZE_K*1024)/2)-0x1000
+#endif
 
 
 typedef enum {
