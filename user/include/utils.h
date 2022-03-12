0a5ba2377bfbc1ecb10d0bdcd9580b2e20d5eb7c
Fedor <lfa1@yandex.ru>
2022-03-12 09:52:49 +0300

    Update utils.h

--- user/include/utils.h
+++ user/include/utils.h
@@ -3,6 +3,6 @@
 
 char* ICACHE_FLASH_ATTR get_ip_address(uint8_t mode);
 char* ICACHE_FLASH_ATTR get_mac_address(uint8_t if_index);
-
+size_t get_sd_free_space();
 
 #endif /* USER_INCLUDE_UTILS_H_ */
