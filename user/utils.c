96055a5d4b4eca337fd1f395b79c4aeff084453f
Fedor <lfa1@yandex.ru>
2022-03-12 09:53:25 +0300

    Update utils.c

--- user/utils.c
+++ user/utils.c
@@ -2,6 +2,7 @@
 #include "user_interface.h"
 
 #include "utils.h"
+#include "ff.h"
 
 static char buff[32] = {0};
 
@@ -23,5 +24,15 @@ char* ICACHE_FLASH_ATTR get_mac_address(uint8_t if_index) {
     os_sprintf(buff, "%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
     return buff;
+}
+
+size_t get_sd_free_space() {
+    FATFS *fs;
+    size_t fre_clust;
+
+    f_getfree("", &fre_clust, &fs);
+
+
+    return ((fre_clust * fs->csize) / 2);
 }
 
