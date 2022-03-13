95fc20185044e8afca79f0a97d107c687ccfb67e
Fedor <lfa1@yandex.ru>
2022-03-13 12:37:05 +0300

    added html files

---
+++ html/scripts.js
@@ -0,0 +1,106 @@
+function setFileName(elem) {
+    var fileName = elem.files[0].name;
+    if (elem.id == "newhtmlfile") {
+        document.getElementById("uploadhtml").value = fileName;
+    } else if (elem.id == "newbinfile") {
+        document.getElementById("uploadbin").value = fileName;
+    }
+}
+
+async function upload(elem) {
+    var fileName = elem.value;
+    var upload_path;
+    var fileInput;
+    var element;
+    if (elem.id == "uploadhtml") {
+        upload_path = "/upload/html/" + fileName;
+        element = document.getElementById("newhtmlfile");
+    } else if (elem.id == "uploadbin") {
+        upload_path = "/upload/image/" + fileName;
+        element = document.getElementById("newbinfile");
+    }
+
+    fileInput = element.files;
+
+    if (fileInput.length == 0) {
+        alert("No file selected!");
+    } else {
+        var file = fileInput[0];
+
+        document.getElementById("uploading").innerHTML = "Uploading! Please wait.";
+        
+        document.getElementById("uploadbin").disabled = true;
+        document.getElementById("uploadhtml").disabled = true;
+        
+        try {
+            var response = await fetch(upload_path, {
+                method: 'POST',
+                body: file
+            });
+            if (response.ok) {
+                var data = await response.text();
+                alert(data);
+            } else {
+                var error = await response.text();
+                var message = `${error}. HTTP error ${response.status}.`;
+                alert(message);
+            }
+            
+        }
+        catch(error) {
+            alert(`Error! ${error}`);
+        }
+        
+        document.getElementById("newbinfile").value = "";
+        document.getElementById("newhtmlfile").value = "";
+
+        location.reload();
+    }
+}
+
+async function listing() {
+    
+    var list_url = "list";
+    
+    try {
+        var response = await fetch(list_url, {
+            method: 'POST'
+        });
+        if (response.ok) {
+            var data = await response.text();
+            document.getElementById("listing").innerHTML = data;
+        } else {
+            var error = await response.text();
+            var message = `${error}. HTTP error ${response.status}.`;
+            alert(message);
+        }
+
+    }
+    catch (error) {
+        alert(`Error! ${error}`);
+    }
+
+}
+
+async function get_ota_filename() {
+    
+    try {
+        var response = await fetch("get_ota_filename");
+        if (response.ok) {
+            var data = await response.json();
+//            var ota_filename = document.getElementById("ota_filename");
+//            ota_filename.value = data.ota_filename;
+    		var output = document.getElementById("ota_filename");
+    		output.innerHTML = data.ota_filename;
+            print_speed(slider.value);
+        } else {
+            var error = await response.text();
+            var message = `${error}. HTTP error ${response.status}.`;
+            alert(message);
+        }
+    }
+    catch (error) {
+        alert(`Error! ${error}`);
+    }
+}
+
